29eb30a5e2d13255ac3c41568e1ffbadeb6a7507
Damir Lutfrakhmanov <60309177+DamirLut@users.noreply.github.com>
2023-08-22 00:23:58 +0500

    :sparkles:feat: revalidate from api

---
+++ pages/api/revalidate.ts
@@ -0,0 +1,19 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  if (req.method !== 'POST') return res.status(404);
+
+  if (req.body.secret !== process.env.REVALIDATE_TOKEN) {
+    return res.status(401).json({ message: 'Invalid token' });
+  }
+
+  try {
+    await res.revalidate(req.body.path);
+    return res.json({ revalidated: true });
+  } catch (err) {
+    return res.status(500).send('Error revalidating');
+  }
+}
